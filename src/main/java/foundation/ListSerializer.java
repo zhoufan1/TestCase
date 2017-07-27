ff101178c180df28d62fa1b16a29018050eae1df
zhoufan <6B23s9pN>
2017-07-27 11:43:31 +0800

    update

---
+++ src/main/java/foundation/ListSerializer.java
@@ -0,0 +1,75 @@
+package foundation;
+
+import com.alibaba.fastjson.serializer.JSONSerializer;
+import com.alibaba.fastjson.serializer.ObjectSerializer;
+import com.alibaba.fastjson.serializer.SerialContext;
+import com.alibaba.fastjson.serializer.SerializeWriter;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.util.TypeUtils;
+import org.apache.commons.lang3.ClassUtils;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+/**
+ * Created by zhoufan on 2017/7/14.
+ */
+public class ListSerializer implements ObjectSerializer {
+    public static final ListSerializer instance = new ListSerializer();
+
+    @Override
+    public void write(JSONSerializer serializer, Object object, Object fieldName, Type fieldType, int features) throws IOException {
+        boolean writeClassName = serializer.out.isEnabled(SerializerFeature.WriteClassName);
+        SerializeWriter out = serializer.out;
+
+        Type elementType = null;
+        if (writeClassName) {
+            elementType = TypeUtils.getCollectionItemType(fieldType);
+        }
+
+        if (object == null) {
+            out.writeNull(SerializerFeature.WriteNullListAsEmpty);
+            return;
+        }
+
+        List<?> list = (List<?>) object;
+
+        if (list.size() == 0) {
+            out.append("[]");
+            return;
+        }
+
+        SerialContext context = serializer.getContext();
+        serializer.setContext(context, object, fieldName, 0);
+        ObjectSerializer itemSerializer = null;
+        try {
+            out.append('[');
+            for (int i = 0, size = list.size(); i < size; ++i) {
+                Object item = list.get(i);
+                if (i != 0) {
+                    out.append(',');
+                }
+
+                if (item == null) {
+                    out.append("null");
+                } else {
+                    Class<?> clazz = item.getClass();
+
+                    if (ClassUtils.isPrimitiveWrapper(clazz)) {
+                        out.writeString(String.valueOf(item));
+                    }
+                    if (serializer.containsReference(item)) {
+                        serializer.writeReference(item);
+                    } else {
+                        itemSerializer = serializer.getObjectWriter(item.getClass());
+                        itemSerializer.write(serializer, item, i, elementType, 0);
+                    }
+                }
+            }
+            out.append(']');
+        } finally {
+            serializer.setContext(context);
+        }
+    }
+}
---
+++ src/main/java/foundation/YppTradeApiApplicationTests.java
@@ -0,0 +1,231 @@
+package foundation;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.ObjectSerializer;
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.SerializeWriter;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.serializer.ValueFilter;
+import com.alibaba.fastjson.util.TypeUtils;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.apache.commons.lang3.ClassUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class YppTradeApiApplicationTests {
+
+    @Test
+    public void test1() {
+        HashMap<String, Object> objectHashMap = Maps.newHashMap();
+        objectHashMap.put("1", BigDecimal.ZERO);
+        objectHashMap.put("2", new Date());
+        objectHashMap.put("3", 3L);
+        objectHashMap.put("4", new Integer(1));
+        objectHashMap.put("5", new Double(5.1));
+        objectHashMap.put("6", Maps.newConcurrentMap());
+        objectHashMap.put("7", Lists.newArrayList());
+        objectHashMap.put("8", Arrays.asList(1, 2, 3));
+        objectHashMap.put("9", Byte.MAX_VALUE);
+        objectHashMap.put("9", Boolean.FALSE);
+
+
+        ValueFilter filter = (object, name, value) -> {
+            System.out.println(value.getClass());
+            if (ClassUtils.isPrimitiveWrapper(value.getClass())) {
+                return String.valueOf(value);
+            } /*else if (value instanceof Collection) {
+                return JsonUtils.toJSONString(value);
+            } else if (value instanceof Objects[]) {
+                return JsonUtils.toJSONString(value);
+            }*/
+
+            return value;
+
+        };
+
+        String s = JSON.toJSONString(objectHashMap, filter, SerializerFeature.SkipTransientField,
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteDateUseDateFormat,
+                SerializerFeature.WriteNonStringValueAsString);
+        System.out.println(s);
+
+
+    }
+
+
+    @Test
+    public void test2() {
+        HashMap<String, Object> objectHashMap = Maps.newHashMap();
+      /*  objectHashMap.put("1", BigDecimal.ZERO);
+        objectHashMap.put("2", new Date());
+        objectHashMap.put("3", 3L);
+        objectHashMap.put("4", new Integer(1));
+        objectHashMap.put("5", new Double(5.1));
+        objectHashMap.put("6", Maps.newConcurrentMap());*/
+        objectHashMap.put("4", new Integer(1));
+        objectHashMap.put("7", Lists.newArrayList(1, 2, 3));
+      /*  objectHashMap.put("8", Arrays.asList(1, 2, 3));
+        objectHashMap.put("9", Byte.MAX_VALUE);
+        objectHashMap.put("9", Boolean.FALSE);*/
+     /*   GodCityDto objectHashMap = new GodCityDto();
+        objectHashMap.setOrderno(1);
+        objectHashMap.setHot(Byte.MAX_VALUE);*/
+        ObjectSerializer objectSerializer = (serializer, object, fieldName, fieldType, features) -> {
+            SerializeWriter out = serializer.getWriter();
+            if (object == null) {
+                out.writeString(StringUtils.EMPTY);
+                return;
+            }
+            if (ClassUtils.isPrimitiveWrapper(TypeUtils.getClass(fieldType))) {
+                out.writeString(String.valueOf(object));
+            }
+            if (fieldType instanceof List) {
+                boolean writeClassName = serializer.out.isEnabled(SerializerFeature.WriteClassName);
+                out.append('[');
+                List list = (List) object;
+                for (int i = 0, size = list.size(); i < size; ++i) {
+                    Object item = list.get(i);
+                    if (i != 0) {
+                        out.append(',');
+                    }
+
+                    if (item == null) {
+                        out.append("null");
+                    } else {
+                        Class<?> clazz = item.getClass();
+/*
+                        if (clazz == Integer.class) {
+                            out.writeInt(((Integer) item).intValue());
+                        } else if (clazz == Long.class) {
+                            long val = ((Long) item).longValue();
+                            if (writeClassName) {
+                                out.writeLong(val);
+                                out.write('L');
+                            } else {
+                                out.writeLong(val);
+                            }
+                        }*/
+                        if (item instanceof Number) {
+                            out.writeString(String.valueOf(item));
+                        }
+                    }
+                }
+                out.append(']');
+            }
+
+        };
+
+        SerializeConfig config = new SerializeConfig();
+      /*  config.put(Integer.class,objectSerializer);
+        config.put(Byte.class,objectSerializer);*/
+        config.put(Collection.class, ListSerializer.instance);
+//        config.put(Boolean.class,objectSerializer);
+        String s = JSON.toJSONString(objectHashMap, config);
+        System.out.println(s);
+
+
+    }
+
+   /* @Test
+    public void test3() throws JsonProcessingException {
+
+        HashMap<String, Object> objectHashMap = Maps.newHashMap();
+        objectHashMap.put("1", BigDecimal.ZERO);
+        objectHashMap.put("2", new Date());
+        objectHashMap.put("3", 3L);
+        objectHashMap.put("4", new Integer(1));
+        objectHashMap.put("5", new Double(5.1));
+        objectHashMap.put("6", Maps.newConcurrentMap());
+        objectHashMap.put("7", Lists.newArrayList());
+        objectHashMap.put("8", Arrays.asList(1, 2, 3));
+        objectHashMap.put("9", Byte.MAX_VALUE);
+        objectHashMap.put("9", Boolean.FALSE);
+
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Number.class, ToStringSerializer.instance);
+        module.addSerializer(Integer.class, ToStringSerializer.instance);
+        module.addSerializer(Boolean.class, ToStringSerializer.instance);
+        module.addSerializer(List.class, ToStringSerializer.instance);
+        module.addSerializer(Byte.class, ToStringSerializer.instance);
+        module.addSerializer(Date.class, DateSerializer.instance);
+        mapper.registerModule(module);
+        System.out.println(mapper.writeValueAsString(objectHashMap));
+    }*/
+
+    @Test
+    public void test4() throws FileNotFoundException {
+
+        File file = new File("D:\\workspace\\companyProjects\\ypp-trade-api\\src\\test\\java\\cn\\yupaopao\\ypptradeapi\\a.txt");
+
+        byte[] bytes = new byte[100];
+        try (InputStream is = new FileInputStream(file)) {
+            while (is.read(bytes) != -1) {
+                System.out.write(bytes);
+                System.out.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test5() throws FileNotFoundException {
+
+        File file = new File("D:\\workspace\\companyProjects\\ypp-trade-api\\src\\test\\java\\cn\\yupaopao\\ypptradeapi\\a.txt");
+
+        char[] chars = new char[100];
+        int charren = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            while ((charren = reader.read(chars)) != -1) {
+                System.out.println(charren);
+                System.out.println(chars);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test6() throws NoSuchFieldException, IllegalAccessException {
+
+      /*  System.out.println(Byte.MAX_VALUE);
+
+        System.out.println((Integer) 100 == (Integer) 100);
+        System.out.println(new Integer(100).equals(new Integer(100)));
+        System.out.println((Integer) 1000 == (Integer) 1000);
+        System.out.println(new Integer(1000).equals(new Integer(1000)));
+*/
+/*
+        Integer a = 1000, b = 1000;
+        System.out.println(a == b);*/
+
+        Class cache = Integer.class.getDeclaredClasses()[0]; //1
+        Field myCache = cache.getDeclaredField("cache"); //2
+        myCache.setAccessible(true);//3
+        Integer[] newCache = (Integer[]) myCache.get(cache); //4
+        newCache[132] = newCache[133]; //5
+        int a = 2;
+        int b = a + a;
+        System.out.printf("%d + %d = %d", a, a, b); //
+    }
+
+}
