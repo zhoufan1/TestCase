d79a86bf7812d75355bf27996841d5b1559d99bc
zhoufan <zhoufan@yupaopao.cn>
2018-01-29 12:26:11 +0800

    update

--- src/main/java/foundation/jackson/YppTradeApiApplicationTests.java
+++ src/main/java/foundation/jackson/YppTradeApiApplicationTests.java
@@ -226,7 +226,8 @@ public class YppTradeApiApplicationTests {
         Field myCache = cache.getDeclaredField("cache"); //2
         myCache.setAccessible(true);//3
         Integer[] newCache = (Integer[]) myCache.get(cache); //4
-        newCache[132] = newCache[133]; //5
+        Integer setCache = newCache[133];
+        newCache[132] = setCache; //5
         int a = 2;
         int b = a + a;
         System.out.printf("%d + %d = %d", a, a, b); //
